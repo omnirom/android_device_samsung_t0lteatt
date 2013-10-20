d4ea837773dbdf1387d54ee25ae46798ab7c320b
UtkarshGupta <utkarsh.eminem@gmail.com>
2013-10-20 18:58:24 +0530

    I317: Add Support
    Change-Id: Ib564ceee3e2f2a3ddca608742dc53c5cfbb05f24

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,4 +15,5 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/full_t0lteatt.mk
+    $(LOCAL_DIR)/full_t0lteatt.mk \
+    $(LOCAL_DIR)/omni_t0lteatt.mk
---
+++ omni_t0lteatt.mk
@@ -0,0 +1,32 @@
+# Copyright (C) 2013 OmniROM Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Grab needed APNs
+PRODUCT_COPY_FILES := vendor/omni/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+# Inherit from our omni product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/t0lteatt/full_t0lteatt.mk)
+
+# Discard inherited values and use our own instead.
+PRODUCT_NAME := omni_t0lteatt
+PRODUCT_DEVICE := t0lteatt
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_MODEL := SGH-I317
+
+# Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=t0lteatt TARGET_DEVICE=t0lteatt BUILD_FINGERPRINT="samsung/t0lteatt/t0lteatt:4.1.2/JZO54K/I317UCAMA4:user/release-keys" PRIVATE_BUILD_DESC="t0lteatt-user 4.1.2 JZO54K I317UCAMA4 release-keys"
